d690f378eb0c442b153cc6e325662a26199474d8
therealedited <43524252+therealedited@users.noreply.github.com>
2021-08-26 18:18:34 +0200

    Add files via upload

--- Polyglot.js
+++ Polyglot.js
@@ -29,6 +29,7 @@
 
 /*
 * Version 1.0 26/08/2021 ~ Polyglot now works.
+* Version 1.1 26/08/2021 ~ It is now possible to change the dub from the pause menu. Language menu added.
 *
 */
 
@@ -36,8 +37,120 @@
 var $Polyglot = null;
 
 function Polyglot() {
+    var value = PluginManager.parameters('Polyglot')['Dubs'];
+    var fontSize = PluginManager.parameters('Polyglot')['Font size'];
+    var parsedValue = JSON.parse(value);
+
     this.currDub = "";
+    this.dubs = parsedValue;
+    this.menuFontSize = 68;
 };
+
+function Scene_Blueprint() {
+    this.initialize.apply(this, arguments);
+};
+
+function Window_Blueprint() {
+    this.initialize.apply(this, arguments);
+};
+
+
+// == Scene ==
+
+Scene_Blueprint.prototype = Object.create(Scene_MenuBase.prototype); //Inherit from whatever type of scene you need here.
+Scene_Blueprint.prototype.constructor = Scene_Blueprint;
+
+Scene_Blueprint.prototype.initialize = function() {
+    Scene_MenuBase.prototype.initialize.call(this);
+};
+
+Scene_Blueprint.prototype.getRealScreenWidth = function() {
+    return window.screen.width * window.devicePixelRatio;
+}
+
+Scene_Blueprint.prototype.getRealScreenHeight = function() {
+    return window.screen.height * window.devicePixelRatio;
+}
+
+Scene_Blueprint.prototype.create = function() {
+    Scene_MenuBase.prototype.create.call(this);
+    var windowWidth = 320;
+    var windowHeight = 240;
+    this._customWindow = new Window_Blueprint((Graphics.boxWidth / 2) - (windowWidth / 2), (Graphics.boxHeight / 2) - (windowHeight / 2), windowWidth, windowHeight);
+    this._customWindow.setHandler("ok", this.reactToOk.bind(this));
+    this._customWindow.setHandler("cancel", this.popScene.bind(this));
+    this.addWindow(this._customWindow);
+};
+
+Scene_Blueprint.prototype.reactToOk = function() {
+    $Polyglot.currDub = $Polyglot.dubs[this._customWindow.index()];
+    SceneManager.pop();
+};
+
+Scene_Blueprint.prototype.start = function() {
+    Scene_MenuBase.prototype.start.call(this);
+
+    this._customWindow.refresh();
+};
+
+// == Scene_Menu ==
+
+var createCommandWindowAlias = Scene_Menu.prototype.createCommandWindow;
+Scene_Menu.prototype.createCommandWindow = function() {
+    createCommandWindowAlias.call(this);
+
+    this._commandWindow.setHandler('polyglot', this.commandPolyglot.bind(this));
+}
+
+Scene_Menu.prototype.commandPolyglot = function() {
+    SceneManager.push(Scene_Blueprint);
+}
+
+
+// == Window ==
+
+Window_Blueprint.prototype = Object.create(Window_Selectable.prototype);
+Window_Blueprint.prototype.constructor = Window_Blueprint;
+
+Window_Blueprint.prototype.initialize = function(x, y, width, height) {
+    Window_Selectable.prototype.initialize.call(this, x, y, width, height);
+
+    this.refresh();
+    this.activate();
+    this.select(0);
+}
+
+Window_Blueprint.prototype.maxItems = function() {
+    return $Polyglot.dubs.length;
+};
+
+Window_Blueprint.prototype.drawItem = function(index) {
+    this.drawDubText(index);
+}
+
+Window_Blueprint.prototype.drawDubText = function(index) {
+    var rect = this.itemRect(index);
+
+    this.changePaintOpacity(false);
+    this.drawTextEx($Polyglot.dubs[index], rect.x + this.padding, rect.y);
+    this.changePaintOpacity(true);
+}
+
+Window_Blueprint.prototype.itemHeight = function() {
+    return Math.floor($Polyglot.menuFontSize / this.numVisibleRows());
+}
+
+Window_Blueprint.prototype.numVisibleRows = function() {
+    return this.maxItems();
+}
+
+// == Window_MenuCommand
+
+var addOriginalCommandsAlias = Window_MenuCommand.prototype.addOriginalCommands;
+Window_MenuCommand.prototype.addOriginalCommands = function() {
+    addOriginalCommandsAlias.call(this);
+    this.addCommand("Languages", "polyglot", true);
+}
 
 // == Save & Load
 var createGameObjectAlias = DataManager.createGameObjects;
@@ -67,7 +180,7 @@ DataManager.extractSaveContents = function(contents) {
 // == Functions ==
 
 function playVoiceline(voicelineName) {
-    var lang = $Polyglot.currDub; //get the language from the player
+    var lang = $Polyglot.currDub;
 
     AudioManager.playSe({name: voicelineName + "_" + lang, volume: 100, pitch:100, pan:0});
 };
@@ -85,7 +198,6 @@ function initDub() {
         }
     )
 }
-
 
 // == Plugin Commands ==
 (function() {
